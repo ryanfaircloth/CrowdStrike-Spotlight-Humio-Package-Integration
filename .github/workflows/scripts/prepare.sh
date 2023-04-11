2d664e18cbe3c06360b31155e3c8a6f44f20604e
Ryan Faircloth <ryan@dss-i.com>
2023-04-11 17:48:26 -0400

    feat: release with ci

--- .github/workflows/scripts/prepare.sh
+++ .github/workflows/scripts/prepare.sh
@@ -3,4 +3,4 @@
 
 # yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
 # helm package chart
-sed -i "s|^__version__.*|__version__ = \"${1}\"|" CrowdStrikeSpotlight2HumioMainV2.py
+sed -i "s|__version__.*|__version__ = \"${1}\"|" CrowdStrikeSpotlight2HumioMainV2.py
